f35ba928f8c4ccd3ffb9f89ce1d7ff2743652a6a
Alexey Taymanov <ataymano@microsoft.com>
2020-04-21 20:43:05 -0400

    dangling rewards match

--- DataScience/LogsWalker/Statistics.py
+++ DataScience/LogsWalker/Statistics.py
@@ -4,6 +4,7 @@ import datetime
 from Helpers import DateTime
 import pandas as pd
 import os
+import sys
 
 def adls_download(adls, adlsPath, localPath, buffersize = 4 * 1024 **2, blocksize = 4 * 1024):
     if os.path.exists(localPath):
@@ -38,12 +39,34 @@ class Statistics:
 
 class SlimLogs:
     @staticmethod
-    def read_dangling_rewards(path, chunksize = None):
-        return pd.read_csv(path, parse_dates=['EnqueuedTimeUtc'], chunksize = chunksize)
+    def __read_dangling_rewards__(path):
+        return pd.read_csv(path, parse_dates=['EnqueuedTimeUtc'])
 
     @staticmethod
-    def read_decisions(path, chunksize = None):
+    def __read_decisions__(path, chunksize = None):
         return pd.read_csv(path, parse_dates=['Timestamp'], chunksize = chunksize)
+
+    @staticmethod
+    def read_dangling_rewards(paths):
+        return pd.concat([SlimLogs.__read_dangling_rewards__(path) for path in paths])
+
+    @staticmethod
+    def read_decisions(paths):
+        return pd.concat([SlimLogs.__read_decisions__(path) for path in paths])
+
+    @staticmethod
+    def iterate_decisions(paths, chunksize):
+        for path in paths:
+            print('\n' + path)
+            for chunk in SlimLogs.__read_decisions__(path, chunksize):
+                sys.stdout.write('.')
+                yield chunk
+
+    @staticmethod
+    def anayze_dangling_rewards(decisions_paths, rewards_paths, chunksize):
+        rewards = SlimLogs.read_dangling_rewards(rewards_paths)['EventId', 'EnqueuedTimeUtc'].groupby('EventId').min()
+#        for decisions in SlimLogs.read_decisions(decisions_paths, chunksize):
+#            joined = 
 
 class StatsContext:
     def __init__(self, local_folder, adlsClient, app, model=None, adls_folder = 'daily'):
@@ -51,11 +74,35 @@ class StatsContext:
         self.AdlsFolder = '{0}/{1}'.format(adls_folder, app)
         self.LocalFolder = local_folder
         self.Model = model
-        os.makedirs(self.LocalFolder, exist_ok=True)
+        self.JoinPath = os.path.join(self.LocalFolder, 'join')
+        os.makedirs(self.JoinPath, exist_ok=True)
 
     @staticmethod
     def __get_path_suffix__(date):
         return '{0}-{1}-{2}'.format(date.year, str(date.month).zfill(2), str(date.day).zfill(2))
+
+    @staticmethod
+    def __match__(decisions, rewards):
+        matched = pd.merge(decisions[['EventId', 'Timestamp']], rewards, how='left', on='EventId')
+        return matched[matched['EnqueuedTimeUtc'].notnull()] 
+
+    def __match_files__(self, decisions_path, rewards_path, decisions_chunk):
+        fname = '{0}-{1}'.format(os.path.basename(decisions_path), os.path.basename(rewards_path))
+        result_path = os.path.join(self.JoinPath, fname)
+        if os.path.exists(result_path):
+            return pd.read_csv(result_path, parse_dates=['EnqueuedTimeUtc', 'Timestamp'])
+        dr = SlimLogs.read_dangling_rewards([rewards_path])[['EventId', 'EnqueuedTimeUtc']].groupby('EventId').min()     
+        result = pd.concat(map(lambda decs: StatsContext.__match__(decs, dr), SlimLogs.iterate_decisions([decisions_path], decisions_chunk)))
+        result.to_csv(result_path, index=False)
+        return result
+    
+    def __match_filesets__(self, decision_paths, reward_paths, decisions_chunk):
+        for decision_path in decision_paths:
+            for reward_path in reward_paths:
+                yield self.__match_files__(decision_path, reward_path, decisions_chunk)
+
+    def match(self, decision_paths, reward_paths, decisions_chunk):
+        return pd.concat(self.__match_filesets__(decision_paths, reward_paths, decisions_chunk))
 
     def __get_file__(self, prefix, date, reset=False):
         suffix = StatsContext.__get_path_suffix__(date)
