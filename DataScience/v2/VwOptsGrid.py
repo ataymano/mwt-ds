d938d26b553840f060b5b5c2f00b4a80c485c954
Alexey Taymanov <ataymano@gmail.com>
2020-05-12 19:02:05 -0400

    vw opts grid

---
+++ DataScience/v2/VwOptsGrid.py
@@ -0,0 +1,45 @@
+import functools
+import itertools
+import json
+import re
+
+
+def apply(first, second):
+    return dict(first, **second)
+
+
+def product(*dimensions):
+    result = functools.reduce(
+        lambda d1, d2: map(
+            lambda tuple: apply(tuple[0], tuple[1]),
+            itertools.product(d1, d2)
+        ), dimensions)
+    return result
+
+
+def dimension(name, values):
+    return list(map(lambda v: dict([(name, str(v))]), values))
+
+
+class Configuration:
+    def __init__(self, name, promote=1, output=1):
+        self.Name = name
+        self.Promote = promote
+        self.Output = output
+
+
+class Grid:
+    def __init__(self, points, config):
+        self.Points = points
+        self.Config = config
+
+
+def generate():
+    hyper_points = product(
+        dimension('-l', [1e-7, 1e-6, 1e-5]),
+    )
+
+    return [
+        Grid(hyper_points, Configuration(name='hyper1', output=2, promote=1)),
+    ]
+
