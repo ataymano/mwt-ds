aa58401e44c36c92a386fd80d9b1c5c805e90982
Alexey Taymanov <ataymano@microsoft.com>
2020-08-12 10:54:56 -0400

    logger cleanup

--- DataScience/v2/VwPipeline/Logger.py
+++ DataScience/v2/VwPipeline/Logger.py
@@ -5,28 +5,27 @@ import sys
 from threading import Lock
 
 class console_logger:
-    def __init__(self, node_id, level='INFO'):
-        self.node_id = node_id
+    def __init__(self, level: str ='INFO'):
         self.level = logging.getLevelName(level)
         self.lock = Lock()
 
-    def debug(self, message):
+    def debug(self, message: str):
         if self.level <= logging.DEBUG: self._trace(message)
 
-    def info(self, message):
+    def info(self, message: str):
         if self.level <= logging.INFO: self._trace(message)
 
-    def warning(self, message):
+    def warning(self, message: str):
         if self.level <= logging.WARNING: self._trace(message)
 
-    def error(self, message):
+    def error(self, message: str):
         if self.level <= logging.ERROR: self._trace(message)
 
-    def critical(self, message):
+    def critical(self, message: str):
         if self.level <= logging.CRITICAL: self._trace(message)
 
-    def _trace(self, message):
-        prefix = '[' + str(self.node_id) + '][' + time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time())) + '] '
+    def _trace(self, message: str):
+        prefix = f'[{time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))}] '
         self.lock.acquire()
         print(prefix + message)
         self.lock.release()
