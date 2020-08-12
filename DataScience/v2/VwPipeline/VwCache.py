a01c320b13ca487aba3f65ac4afae5fabb31b866
Alexey Taymanov <ataymano@microsoft.com>
2020-08-12 10:31:36 -0400

    VwCache + test

---
+++ DataScience/v2/VwPipeline/VwCache.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+import VwOpts
+
+class VwCache:
+    def __init__(self, path: str, create=True):
+        self.Path = path
+        if create:
+            os.makedirs(self.Path, exist_ok=True)
+
+    @staticmethod
+    def __file_name__(string_hash: str) -> str:
+        import hashlib
+        return hashlib.md5(string_hash.encode('utf-8')).hexdigest()
+
+    def __make_path__(self, context: str, args_hash: str) -> str:
+        folder_name = os.path.join(self.Path, context)
+        os.makedirs(folder_name, exist_ok=True)
+        return os.path.join(folder_name, VwCache.__file_name__(args_hash))
+
+    def get_path(self, opts_in: dict, opt_out: str = None) -> str:
+        args_hash = VwOpts.string_hash(VwOpts.to_string(opts_in))
+        return self.__make_path__(f'populate-{opt_out}', args_hash)
+
+    
+
+
