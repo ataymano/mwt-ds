e845efed5a0df3aa4aa1161c6ec861a7bc26315f
Alexey Taymanov <ataymano@microsoft.com>
2020-08-12 16:10:55 -0400

    pool cleanup

--- DataScience/v2/VwPipeline/Pool.py
+++ DataScience/v2/VwPipeline/Pool.py
@@ -2,7 +2,7 @@ import multiprocessing
 from multiprocessing.pool import ThreadPool
 
 
-def execute(task_index_input):
+def __execute__(task_index_input):
     return task_index_input[1], task_index_input[0](*task_index_input[2])
 
 
@@ -21,7 +21,7 @@ class MultiThreadPool:
     def map(self, task, inputs):
         p = ThreadPool(processes=self.Procs)
         args = [(task, index, input) for index, input in enumerate(inputs)]
-        result = p.imap_unordered(execute, args)
+        result = p.imap_unordered(__execute__, args)
         p.close()
         p.join()
         outputs = [r[1] for r in sorted(result, key=lambda item: item[0])]
