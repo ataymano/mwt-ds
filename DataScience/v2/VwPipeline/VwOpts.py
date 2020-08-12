338d260e5a691cda5584c76cc81bf0d691cb251f
Alexey Taymanov <ataymano@microsoft.com>
2020-08-11 22:53:19 -0400

    VwOpts test cleanup

--- DataScience/v2/VwPipeline/VwOpts.py
+++ DataScience/v2/VwPipeline/VwOpts.py
@@ -1,4 +1,8 @@
-def normalize(cmd):
+def string_hash(cmd: str) -> str:
     items = [i.strip() for i in f' {cmd}'.split(' -')]
     items.sort()
     return ' '.join(items).strip()
+
+def to_string(opts: dict) -> str:
+    return ' '.join(['{0} {1}'.format(key, opts[key]) if not key.startswith('#')
+        else str(opts[key]) for key in sorted(opts.keys())])
--- DataScience/v2/VwPipeline/tests/test_VwOpts.py
+++ DataScience/v2/VwPipeline/tests/test_VwOpts.py
@@ -1,32 +1,49 @@
 import unittest
 import VwOpts
 
-class TestNormalize(unittest.TestCase):
+class TestStringHash(unittest.TestCase):
     def test_equal_after_normalize(self):
         self.assertEqual(
-            VwOpts.normalize('--ccb_explore_adf --epsilon 0.1 --dsjson'),
-            VwOpts.normalize(' --ccb_explore_adf --dsjson --epsilon 0.1'))
+            VwOpts.string_hash('--ccb_explore_adf --epsilon 0.1 --dsjson'),
+            VwOpts.string_hash(' --ccb_explore_adf --dsjson --epsilon 0.1'))
 
         self.assertEqual(        
-            VwOpts.normalize('--ccb_explore_adf --epsilon 0.1 --dsjson'),
-            VwOpts.normalize('--dsjson  --ccb_explore_adf  --epsilon 0.1  '))
+            VwOpts.string_hash('--ccb_explore_adf --epsilon 0.1 --dsjson'),
+            VwOpts.string_hash('--dsjson  --ccb_explore_adf  --epsilon 0.1  '))
 
         self.assertEqual(
-            VwOpts.normalize('--ccb_explore_adf --epsilon 0.1 --dsjson --l 0.2'),
-            VwOpts.normalize('--dsjson  --ccb_explore_adf --l 0.2 --epsilon 0.1  '))
+            VwOpts.string_hash('--ccb_explore_adf --epsilon 0.1 --dsjson --l 0.2'),
+            VwOpts.string_hash('--dsjson  --ccb_explore_adf --l 0.2 --epsilon 0.1  '))
 
     def test_not_equal_after_normalize(self):
         self.assertNotEqual(
-            VwOpts.normalize('--ccb_explore_adf --epsilon 0.1 --dsjson'),
-            VwOpts.normalize(' --ccb_explore_adf --dsjson --epsilon 0.2'))
+            VwOpts.string_hash('--ccb_explore_adf --epsilon 0.1 --dsjson'),
+            VwOpts.string_hash(' --ccb_explore_adf --dsjson --epsilon 0.2'))
 
         self.assertNotEqual(        
-            VwOpts.normalize('--ccb_explore_adf --epsilon 0.1 --dsjson'),
-            VwOpts.normalize('--dsjson  --cb_explore_adf  --epsilon 0.1  '))
+            VwOpts.string_hash('--ccb_explore_adf --epsilon 0.1 --dsjson'),
+            VwOpts.string_hash('--dsjson  --cb_explore_adf  --epsilon 0.1  '))
 
         self.assertNotEqual(
-            VwOpts.normalize('--ccb_explore_adf --epsilon 0.1 --dsjson --l 0.2'),
-            VwOpts.normalize('--dsjson  --ccb_explore_adf --l 0.1 --epsilon 0.2  '))
+            VwOpts.string_hash('--ccb_explore_adf --epsilon 0.1 --dsjson --l 0.2'),
+            VwOpts.string_hash('--dsjson  --ccb_explore_adf --l 0.1 --epsilon 0.2  '))
+
+class TestToString(unittest.TestCase):
+    def test_to_string(self):
+        self.assertEqual(
+            VwOpts.to_string({'#base': '--ccb_explore_adf --epsilon 0.1 --dsjson'}),
+            '--ccb_explore_adf --epsilon 0.1 --dsjson')
+
+        self.assertEqual(
+            VwOpts.to_string({'#base': '--ccb_explore_adf --epsilon 0.1 --dsjson',
+                                '--l': 0.1}),
+            '--ccb_explore_adf --epsilon 0.1 --dsjson --l 0.1')
+
+        self.assertEqual(
+            VwOpts.to_string({'#base': '--ccb_explore_adf --epsilon 0.1 --dsjson',
+                                '--l': 0.1,
+                                '--cb_type': 'mtr'}),
+            '--ccb_explore_adf --epsilon 0.1 --dsjson --cb_type mtr --l 0.1')
 
 if __name__ == '__main__':
     unittest.main()
